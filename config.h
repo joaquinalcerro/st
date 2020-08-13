a92ef9a2d5b8ad94c6644e2093840fe2679f4dd3
Joaquin Alcerro <joaquin@nimbbo.com>
2020-08-12 22:23:28 -0600

    Font and keyboard bindings customization

--- config.h
+++ config.h
@@ -5,8 +5,11 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "mono:pixelsize=14:antialias=true:autohint=true";
-static char *font2[] = { "JoyPixels:pixelsize=10:antialias=true:autohint=true" };
+static char *font = "JetBrains Mono Regular:pixelsize=16:antialias=true:autohint=true";
+static char *font2[] = { 
+  "DejaVu Sans Mono for Powerline:pixelsize=16:antialias=true:autohint=true",
+  "Symbola:pixelsize=16:antialias=true:autohint=true"
+};
 static int borderpx = 2;
 
 /*
@@ -224,7 +227,8 @@ static MouseShortcut mshortcuts[] = {
 
 /* Internal keyboard shortcuts. */
 #define MODKEY Mod1Mask
-#define TERMMOD (Mod1Mask|ShiftMask)
+/* #define TERMMOD (Mod1Mask|ShiftMask) */
+#define TERMMOD (ControlMask|ShiftMask)
 
 MouseKey mkeys[] = {
 	/* button               mask            function        argument */
@@ -253,10 +257,10 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Prior,       zoom,           {.f = +1} },
 	{ TERMMOD,              XK_Next,        zoom,           {.f = -1} },
 	{ MODKEY,               XK_Home,        zoomreset,      {.f =  0} },
-	{ MODKEY,               XK_c,           clipcopy,       {.i =  0} },
+	{ TERMMOD,              XK_C,           clipcopy,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      clippaste,      {.i =  0} },
-	{ MODKEY,               XK_v,           clippaste,      {.i =  0} },
-	{ XK_ANY_MOD,		Button2,	selpaste,	{.i =  0} },
+	{ TERMMOD,               XK_V,           clippaste,      {.i =  0} },
+	{ XK_ANY_MOD,		        Button2,	selpaste,	{.i =  0} },
 	{ MODKEY,               XK_Num_Lock,    numlock,        {.i =  0} },
 	{ MODKEY,               XK_Control_L,   iso14755,       {.i =  0} },
 	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
